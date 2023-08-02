178829b6fca9bed8e220c9616c8ad01549343376
mided3v <ayo4real6@gmail.com>
2023-08-02 16:12:19 +0100

    header component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,35 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { HiMenu } from "react-icons/hi";
-import {Link} from 'react-router-dom'
+import { GrClose } from "react-icons/gr";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const [headerBgColor, setHeaderBgColor] = useState("");
+
+  const listenScrollEvent = (e) => {
+      e.preventDefault();
+
+    if (window.scrollY < 750) {
+      return  setHeaderBgColor("");
+    } else {
+      return  setHeaderBgColor("darkgreen");
+    }
+  };
+
+useEffect(() => {
+  window.addEventListener("scroll", listenScrollEvent);
+
+  return () => window.removeEventListener("scroll", listenScrollEvent);
+});
+  
+  
+  
   return (
-    <div id="header">
+    <div id="header" style={{ backgroundColor: headerBgColor }}>
       <div id="logo">
-        <a href="#"> mideBillions</a>
+        <a href="#"> ayoBilli0ns</a>
       </div>
       <ul className="nav-options">
         <li>
           {/* <Link to="/">Home</Link> */}
-          <a href=""> Home</a>
+          <a href="#"> Home</a>
+        </li>
+        <li>
+          <a href="#projects"> Projects</a>
         </li>
         <li>
           {" "}
           <a href="#about"> About</a>
         </li>
         {/* <li>Services</li> */}
-        <li>
-          <a href="#projects"> Projects</a>
-        </li>
+
         <li>
           <a href="#contact"> Contact</a>
         </li>
       </ul>
-      <div className="nav-menu">
+      <div
+        className="nav-menu"
+        onClick={() => {
+          setOpenMenu(!openMenu);
+        }}
+      >
         <HiMenu />
+        {/* <GrClose/> */}
       </div>
     </div>
   );
-}
+};
 
-export default Header
+export default Header;
