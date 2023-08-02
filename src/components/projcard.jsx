65a2b094d47795f18807d76e0a8ccbb873b2df38
mided3v <ayo4real6@gmail.com>
2023-08-02 16:12:32 +0100

    component for project card

--- src/components/projcard.jsx
+++ src/components/projcard.jsx
@@ -1,16 +1,56 @@
-import React from 'react'
-import '../App.css'
-import Dodov from '../asset/dodov.png'
-const Projcard = () => {
+import React from "react";
+import "../App.css";
+import Dodov from "../asset/dodov.png";
+import ImageSample from "../asset/engineer.jpeg";
+const Projcard = ({ image, title, description, tools, link, link2 }) => {
   return (
-    <div className="card">
-      <img src={Dodov} alt="" />
-      <div className="proj-desc">
-        <h3>Porfolio for Hristiyan Dodov</h3>
-      
-      <p>Description: Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p></div>
-    </div>
-  );
-}
+    <>
+      {" "}
+      <div className="card">
+        <img src={image} alt="" />
+        <div className="proj-desc">
+          <h3>{title}</h3>
+          <p>{description}</p>
+          <h5>{tools}</h5>
+          <div className="card-btn">
+            <a href={link} target="_blank" rel="noopener noreferrer">
+              View Site
+            </a>
+            <a href={link2} target="_blank" rel="noopener noreferrer">
+              View Code
+            </a>
+          </div>
+        </div>
+      </div>
 
-export default Projcard
+      
+      {/* <div className="card card2">
+        <img src={image} alt="" />
+
+        <div className="proj-desc">
+          <h3>{title}</h3>
+          <p>{description}</p>
+          <h5>{tools}</h5>
+          <div className="card-btn">
+            <a href={link} target="_blank" rel="noopener noreferrer">
+              View Site
+            </a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              View Code
+            </a>
+          </div>
+        </div>
+      </div> */}
+    </>
+  );
+};
+Projcard.defaultProps = {
+  image: ImageSample,
+  title: "New project",
+  description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit",
+  tools: "Tools: HTML, CSS, JS",
+  link: "https://www.google.com/",
+  link2: "https://www.google.com/",
+};
+
+export default Projcard;
