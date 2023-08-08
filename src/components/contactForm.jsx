98b353ad9f22a428311a7daff51e1a63aa1626a6
mided3v <ayo4real6@gmail.com>
2023-08-08 09:12:01 +0100

    replaced contact form with EmailJS form

--- src/components/contactForm.jsx
+++ src/components/contactForm.jsx
@@ -1,52 +1,60 @@
-import React from "react";
-import { useState } from "react";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
-const ContactForm = () => {
-  const [name, setName] = useState("Banji");
-  const [email, setEmail] = useState("ay.com");
-  const handleSubmit = (e) => {
-    alert(`SUCCESS! name: ${name}, email: ${email}`);
+export const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
     e.preventDefault();
+    alert('SUCCESS!');
+    emailjs
+      .sendForm(
+        "service_4gv6ola",
+        "template_futfc6n",
+        form.current,
+        "1lOL10nl91Hv2TJfK"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("Successful!" );
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
+
   return (
-    <form action="" method="get" onSubmit={handleSubmit}>
-      <label htmlFor="name">
-        {" "}
-        Name
-        <input
-          type="text"
-          name="fullname"
-          id="name"
-          placeholder="Please enter your full name"
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
-      <label htmlFor="email">
-        {" "}
-        Email
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Please enter your email address"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
-
-      <label className="textlabel" htmlFor="message">
-        Message
-        <textarea
-          name="message"
-          id="message"
-          cols="30"
-          rows="10"
-          placeholder="Enter your message here"
-        ></textarea>
-      </label>
-
-      <button type="submit">Submit</button>
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name:</label>
+      <input
+        type="text"
+        name="user_name"
+        id="name"
+        placeholder="Please enter your full name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <label>Email:</label>
+      <input
+        type="email"
+        name="user_email"
+        id="email"
+        placeholder="Please enter your email address"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <label>Message:</label>
+      <textarea
+        name="message"
+        id="message"
+        cols="30"
+        rows="10"
+        placeholder="Enter your message here"
+      />
+      <input type="submit" value="Send" id="send-btn" />
     </form>
   );
 };
-
-export default ContactForm;
